d72f2067cf88881f998b7b5999a1d78758a87402
Kevin Trebossen <kevintrebossen@imac-de-kevin.local>
2018-09-23 00:15:12 +0200

    Initial Commit

---
+++ Console Retro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Console Retro
+//
+//  Created by Kevin Trebossen on 23/09/18.
+//  Copyright © 2018 KTD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
