dc8633c74fd735ebeff47224a64693c5bea6d2ce
Kevin Trebossen <kevintrebossen@imac-de-kevin.local>
2018-09-23 20:03:49 +0200

    Liaison des données avec le nouveau controller

--- Console Retro/DetailController.swift
+++ Console Retro/DetailController.swift
@@ -9,13 +9,23 @@
 import UIKit
 
 class DetailController: UIViewController {
-
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var retroIV: UIImageView!
+    @IBOutlet weak var dureeLabel: UILabel!
+    @IBOutlet weak var descTextField: UITextView!
+    
     var console: Console?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         guard console != nil else { return }
         title = console!.nom
+        titleLabel.text = console!.nom
+        retroIV.image = console!.image
+        dureeLabel.text = console?.cycleDeVie()
+        descTextField.text = console?.desc
+        view.backgroundColor = console?.couleur
     }
 
 }
